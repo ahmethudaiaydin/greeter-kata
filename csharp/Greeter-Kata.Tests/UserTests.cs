b9bc0c7f7e4c340b2826506f9d10b7fb9fadefe1
Ahmet Hudai Aydin <ahmet.aydin@authoritypartners.com>
2023-11-06 14:39:26 +0300

    session-2

---
+++ csharp/Greeter-Kata.Tests/UserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Greeter_Kata.Tests
+{
+    public class UserTests
+    {
+
+        [Fact]
+        public void ShouldReturnFullUsername() {
+
+            var user = new User("John", "Doe", DateTime.Now.AddYears(-30));
+            var fullname = user.GetUserName();
+
+            Assert.Equal("John Doe", fullname);
+        }
+    }
+}
